a4fd1b1ae2bdf07826f9d56f426bd534ce7129c4
Dylan L <dylancblarocque@gmail.com>
2023-04-01 23:38:49 -0700

    accept step as the action

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,14 +3,19 @@
 
 import * as React from 'react'
 
-function countReducer(state, newState) {
-  return newState
+// function countReducer(state, newState) {
+//   return newState
+// }
+function countReducer(count, step) {
+  return count + step
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  // const [count, setCount] = React.useReducer(countReducer, initialCount)
+  const [count, chnageCount] = React.useReducer(countReducer, initialCount)
 
-  const increment = () => setCount(count + step)
+  // const increment = () => setCount(count + step)
+  const increment = () => chnageCount(step)
   return <button onClick={increment}>{count}</button>
 }
 
